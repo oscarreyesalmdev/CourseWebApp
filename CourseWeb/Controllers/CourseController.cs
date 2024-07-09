1619116d0ba21c72904da76184a3f70c2182df01
Steilor <steilormateo.44@gmail.com>
2024-07-09 18:22:42 -0400

    Course controller

---
+++ CourseWeb/Controllers/CourseController.cs
@@ -0,0 +1,94 @@
+﻿using CourseWeb.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace CourseWeb.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly ILogger<CourseController> _logger;
+        private readonly CourseAppContext _context;
+        public CourseController(ILogger<CourseController> logger, CourseAppContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+        // GET: CourseController
+        public ActionResult Index()
+        {
+            var curso = _context.Cursos.ToList();
+            return View(curso);
+        }
+
+        // GET: CourseController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CourseController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CourseController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CourseController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CourseController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CourseController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CourseController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
