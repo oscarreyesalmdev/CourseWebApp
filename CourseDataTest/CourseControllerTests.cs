ba4dd0fef5815971d0e93065bee7861ea85873dc
EduardoMartine <whatdice26@gmail.com>
2024-07-10 23:33:59 -0400

    Pruebas Unitarias 1

---
+++ CourseDataTest/CourseControllerTests.cs
@@ -0,0 +1,82 @@
+﻿using CourseWeb.Controllers;
+using CourseWeb.Data;
+using CourseWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CourseWeb.Tests
+{
+    public class CourseControllerTests
+    {
+        private readonly DbContextOptions<CourseAppContext> _options;
+        private CourseAppContext _context;
+
+        public CourseControllerTests()
+        {
+            _options = new DbContextOptionsBuilder<CourseAppContext>()
+                .UseInMemoryDatabase(databaseName: "CourseAppDatabase")
+                .Options;
+
+            _context = new CourseAppContext(_options);
+        }
+
+        private void SeedDatabase()
+        {
+            _context.Cursos.AddRange(
+                new Curso { Titulo = "Curso 1", Descripcion = "Descripción 1", FechaPublicacion = System.DateTime.Now },
+                new Curso { Titulo = "Curso 2", Descripcion = "Descripción 2", FechaPublicacion = System.DateTime.Now }
+            );
+            _context.SaveChanges();
+        }
+
+        [Fact]
+        public async Task CrearC_Post_ReturnsRedirectToIndex_WhenModelIsValid()
+        {
+            // Arrange
+            _context.Database.EnsureDeleted();
+            _context = new CourseAppContext(_options);
+            SeedDatabase();
+
+            var controller = new CourseController(null, _context);
+
+            var newCourse = new Curso
+            {
+                Titulo = "Nuevo Curso",
+                Descripcion = "Nueva Descripción",
+                FechaPublicacion = System.DateTime.Now
+            };
+
+            // Act
+            var result = await controller.CrearC(newCourse);
+
+            // Assert
+            var redirectToActionResult = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("Index", redirectToActionResult.ActionName);
+        }
+
+        [Fact]
+        public void Index_ReturnsViewResult_WithListOfCourses()
+        {
+            // Arrange
+            _context.Database.EnsureDeleted();
+            _context = new CourseAppContext(_options);
+            SeedDatabase();
+
+            var controller = new CourseController(null, _context);
+
+            // Act
+            var result = controller.Index();
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<IEnumerable<Curso>>(viewResult.ViewData.Model);
+            Assert.Equal(2, model.Count());
+        }
+
+       
+    }
+}
--- CourseDataTest/CourseTest.cs
+++ CourseDataTest/CourseTest.cs
@@ -1,15 +1,130 @@
 using CourseWeb.Controllers;
 using CourseWeb.Data;
+using CourseWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
 
-namespace CourseDataTest
+public class CourseControllerTests
 {
-    public class CourseTest(CourseAppContext context)
+    private readonly Mock<ILogger<CourseController>> _loggerMock;
+
+    public CourseControllerTests()
     {
-        [Fact]
-        public void EditarNullCourse()
+        _loggerMock = new Mock<ILogger<CourseController>>();
+    }
+
+    private CourseAppContext GetInMemoryContext()
+    {
+        var options = new DbContextOptionsBuilder<CourseAppContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString()) // Use a new database instance for each test
+            .Options;
+
+        return new CourseAppContext(options);
+    }
+
+    [Fact]
+    public void Index_ReturnsViewResult_WithListOfCourses()
+    {
+        // Arrange
+        var context = GetInMemoryContext();
+        var cursos = new List<Curso>
         {
-            
-            
-        }
+            new Curso { Id = 1, Titulo = "Curso 1", Descripcion = "Descripcion 1", FechaPublicacion = DateTime.Now },
+            new Curso { Id = 2, Titulo = "Curso 2", Descripcion = "Descripcion 2", FechaPublicacion = DateTime.Now }
+        };
+        context.Cursos.AddRange(cursos);
+        context.SaveChanges();
+
+        var controller = new CourseController(_loggerMock.Object, context);
+
+        // Act
+        var result = controller.Index();
+
+        // Assert
+        var viewResult = Assert.IsType<ViewResult>(result);
+        var model = Assert.IsAssignableFrom<List<Curso>>(viewResult.ViewData.Model);
+        Assert.Equal(2, model.Count);
+    }
+
+    [Fact]
+    public async Task CrearC_ValidModel_RedirectsToIndex()
+    {
+        // Arrange
+        var context = GetInMemoryContext();
+        var curso = new Curso { Titulo = "Curso 1", Descripcion = "Descripcion 1", FechaPublicacion = DateTime.Now };
+        var controller = new CourseController(_loggerMock.Object, context);
+
+        // Act
+        var result = await controller.CrearC(curso);
+
+        // Assert
+        var redirectToActionResult = Assert.IsType<RedirectToActionResult>(result);
+        Assert.Equal("Index", redirectToActionResult.ActionName);
+        Assert.Equal(1, context.Cursos.Count());
+        Assert.Equal(curso, context.Cursos.First());
+    }
+
+    [Fact]
+    public async Task CrearC_InvalidModel_ReturnsViewWithModelError()
+    {
+        // Arrange
+        var context = GetInMemoryContext();
+        var curso = new Curso { Titulo = new string('A', 41), Descripcion = "Descripcion 1", FechaPublicacion = DateTime.Now };
+        var controller = new CourseController(_loggerMock.Object, context);
+        controller.ModelState.AddModelError("Titulo", "El título no puede tener más de 40 caracteres.");
+
+        // Act
+        var result = await controller.CrearC(curso);
+
+        // Assert
+        var viewResult = Assert.IsType<ViewResult>(result);
+        var model = Assert.IsAssignableFrom<Curso>(viewResult.ViewData.Model);
+        Assert.Equal(curso, model);
+        Assert.True(controller.ModelState.ContainsKey("Titulo"));
+    }
+
+    [Fact]
+    public async Task EditarC_GetValidId_ReturnsViewWithCourse()
+    {
+        // Arrange
+        var context = GetInMemoryContext();
+        var curso = new Curso { Id = 1, Titulo = "Curso 1", Descripcion = "Descripcion 1", FechaPublicacion = DateTime.Now };
+        context.Cursos.Add(curso);
+        context.SaveChanges();
+        var controller = new CourseController(_loggerMock.Object, context);
+
+        // Act
+        var result = await controller.EditarC(1);
+
+        // Assert
+        var viewResult = Assert.IsType<ViewResult>(result);
+        var model = Assert.IsAssignableFrom<Curso>(viewResult.ViewData.Model);
+        Assert.Equal(curso, model);
+    }
+
+    [Fact]
+    public async Task EliminarC_ValidId_RedirectsToIndex()
+    {
+        // Arrange
+        var context = GetInMemoryContext();
+        var curso = new Curso { Id = 1, Titulo = "Curso 1", Descripcion = "Descripcion 1", FechaPublicacion = DateTime.Now };
+        context.Cursos.Add(curso);
+        context.SaveChanges();
+        var controller = new CourseController(_loggerMock.Object, context);
+
+        // Act
+        var result = await controller.EliminarC(1);
+
+        // Assert
+        var redirectToActionResult = Assert.IsType<RedirectToActionResult>(result);
+        Assert.Equal("Index", redirectToActionResult.ActionName);
+        Assert.Empty(context.Cursos);
     }
 }
